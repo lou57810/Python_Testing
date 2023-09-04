67f342054eda17fdb4da16aac2262e080007e7b2
Bernard Oswald <bernard.oswald@free.fr>
2023-09-05 00:15:33 +0200

    Test_unitaires_1

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,8 +1,7 @@
 import pytest
-# from Python_Testing.tests.unit_tests import clubs
-# from Python_Testing.tests.unit_tests import competitions
-
+# import Python_Testing.server
 from Python_Testing.server import app
+
 
 
 @pytest.fixture
@@ -10,3 +9,64 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
+
+mock_clubs = [
+    {
+        "name": "Héraclès_Temple",
+        "email": "hercule@heavylift.gr",
+        "points": "18"
+    },
+    {
+        "name": "Popeye_Loft",
+        "email": "pop@spinach.com",
+        "points": "15"
+    },
+    {
+        "name": "Serena_Lounge",
+        "email": "venus@sheace.us",
+        "points": "14"
+    }
+]
+# mocker_clubs = [Club(**club) for club in mock_clubs]
+
+mock_comps = [
+        {
+            "name": "Summer LiftFest",
+            "date": "2023-04-20 15:00:00",
+            "numberOfPlaces": "35"
+        },
+        {
+            "name": "Canadian Lifter",
+            "date": "2023-10-15 16:30:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Australian Lifter",
+            "date": "2023-11-20 18:30:00",
+            "numberOfPlaces": "11"
+        }
+    ]
+"""
+mocker_comps = [Competition(**comp) for comp in mock_comps]
+
+mocker_data = Data(None, None)
+mocker_data.clubs = [Club(**club) for club in mock_clubs]
+mocker_data.competitions = [Competition(**comp) for comp in mock_comps]
+
+
+@pytest.fixture
+def app(mocker):
+    mocker.patch.object(server, "COMPETITIONS", test_comp())
+    mocker.patch.object(server, "CLUBS", test_club())
+    return create_app({"TESTING": True})
+
+
+@pytest.fixture
+def loadLocalClubJson():
+    w_dir = os.path.abspath('.')
+    if os.path.isfile(os.path.join(w_dir)):
+        print('os.path')
+        print(w_dir)
+        return os.path
+"""
--- tests/unit_tests/test_units.py
+++ tests/unit_tests/test_units.py
@@ -1,66 +1,33 @@
+import pytest
 import requests
 import unittest
+import json
+
 from .conftest import client
 from unittest import mock, TestCase
 from unittest.mock import patch, Mock
 from Python_Testing import server
-from Python_Testing.server import loadClubs
+from Python_Testing.server import loadClubs, clubs, competitions
 from Python_Testing.server import loadCompetitions
+from flask import Flask
+import os
 
-
-# constants
-PATH_COMPETITIONS_FILE = "tests/unit_test/competitions.json"
-PATH_CLUBS_FILE = "tests/unit_tests/clubs.json"
-
-
-# server.clubs = [
-data_clubs = [
-    {
-        "name":"Héraclès_Temple",
-        "email":"hercule@heavylift.gr",
-        "points":"18"
-    },
-    {
-        "name":"Popeye_Loft",
-        "email": "pop@spinach.com",
-        "points":"15"
-    },
-    {   "name":"Serena_Lounge",
-        "email": "venus@sheace.us",
-        "points":"14"
-    }
-]
-
-server.competitions = [
-    {
-        "name": "Summer Meeting",
-        "date": "2023-04-20 15:00:00",
-        "numberOfPlaces": "35"
-    },
-    {
-        "name": "Canadian Lifter",
-        "date": "2023-11-20 18:30:00",
-        "numberOfPlaces": "25"
-    }
-]
-    
- 
 
 # ============ Logging ==============
 
 def test_index_email_ok(client):
-	response = client.get('/')
-	assert response.status_code == 200
-	data = response.data.decode()
-	assert "Welcome" in data
+    response = client.get('/')
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert "Welcome" in data
 
 
 def test_index_email_wrong(client):
-	email = "unknown@email.com"
-	response = client.post("/showSummary", data={"email": email})
-	assert response.status_code == 200
-	data = response.data.decode()
-	assert "Wrong Email Try again" in data
+    email = "unknown@email.com"
+    response = client.post("/showSummary", data={"email": email})
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert "Wrong Email Try again" in data
 
 
 def test_logout_page(client):
@@ -68,27 +35,88 @@ def test_logout_page(client):
     data = response.data.decode()
     assert response.status_code == 302
 
-
+"""
 # ====Test global Fonction load_clubs() & load_competitions() ====
 
-
-
-
-def test_load_clubs_file_not_empty():
-    """ Test that clubs database is not empty """
-    clubs = open
+def test_load_clubs_file():
+    # Test load function return true values
+     
     data_clubs = loadClubs()
-    print('data0:', data_clubs[0]['email'])
-    assert data_clubs[0]['email'] == 'john@simplylift.co'    
-    assert data_clubs != ""
+    assert data_clubs[0]['name'] == "Simply Lift"
+    assert data_clubs[0]['email'] == 'john@simplylift.co'
+    assert data_clubs[0]['points'] == "13"
+    assert data_clubs[1]['name'] == "Iron Temple"
+    assert data_clubs[1]['email'] == 'admin@irontemple.com'
+    assert data_clubs[1]['points'] == "4"
+    assert data_clubs[2]['name'] == "She Lifts"
+    assert data_clubs[2]['email'] == 'kate@shelifts.co.uk'
+    assert data_clubs[2]['points'] == "12"
 
 
-def test_load_competitions_file_not_empty():
-    """ Test that clubs database is not empty """
-    
+def test_load_competitions_file():
+    # Test load function return true values 
     data_competitions = loadCompetitions()
-    assert data_competitions != ""
-
+    assert data_competitions[0]['name'] == "Winter Meeting"
+    assert data_competitions[0]['date'] == "2020-04-20 15:00:00"
+    assert data_competitions[0]['numberOfPlaces'] == "28"
+    assert data_competitions[1]['name'] == "HeroesLifter"
+    assert data_competitions[1]['date'] == "2022-11-20 18:30:00"
+    assert data_competitions[1]['numberOfPlaces'] == "18"
 
 
 # ====Test local Fonction load_clubs() & load_competitions() ====
+
+def test_local_loadClubs():
+    pass
+    
+def test_local_loadCompetitions():
+    pass
+"""
+# ================================================================
+""" test messages of usage of points and places """
+
+
+class TestLoadClubs(unittest.TestCase):
+    @patch('Python_Testing.server.loadClubs')
+    def test_load_clubs_response(self, mock_get_club_json):
+        mock_get_club_json.return_value = [
+    {
+        "name": "Héraclès_Temple",
+        "email": "hercule@heavylift.gr",
+        "points": "18"
+    },
+    {
+        "name": "Popeye_Loft",
+        "email": "pop@spinach.com",
+        "points": "15"
+    },
+    {
+        "name": "Serena_Lounge",
+        "email": "venus@sheace.us",
+        "points": "14"
+    }
+]
+        self.assertEqual(loadClubs(), clubs)
+
+
+class TestLoadCompetitions(unittest.TestCase):
+    @patch('Python_Testing.server.loadCompetitions')
+    def test_load_competitions_response(self, mock_get_comp_json):
+        mock_get_comp_json.return_value = [
+        {
+            "name": "Summer LiftFest",
+            "date": "2023-04-20 15:00:00",
+            "numberOfPlaces": "35"
+        },
+        {
+            "name": "Canadian Lifter",
+            "date": "2023-10-15 16:30:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Australian Lifter",
+            "date": "2023-11-20 18:30:00",
+            "numberOfPlaces": "11"
+        }
+    ]
+        self.assertEqual(loadCompetitions(), competitions)
