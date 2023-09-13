1b100009df586253fea6b4beae4ce049e43cc052
Bernard Oswald <bernard.oswald@free.fr>
2023-09-13 18:25:56 +0200

    Ajout tests accès pages + contrôle points et places.

--- tests/unit_tests/test_units.py
+++ tests/unit_tests/test_units.py
@@ -10,6 +10,7 @@ from unittest.mock import patch, Mock, MagicMock
 from Python_Testing import server
 from Python_Testing.server import loadClubs, clubs, competitions
 from Python_Testing.tests.unit_tests.conftest import mock_clubs, mock_comps
+
 from Python_Testing.server import loadCompetitions
 from flask import Flask
 import os
@@ -37,50 +38,8 @@ def test_logout_page(client):
     data = response.data.decode()
     assert response.status_code == 302
 
-"""
-# ====Test global Fonction load_clubs() & load_competitions() ====
 
-def test_load_clubs_file():
-    # Test load function return true values
-     
-    data_clubs = loadClubs()
-    assert data_clubs[0]['name'] == "Simply Lift"
-    assert data_clubs[0]['email'] == 'john@simplylift.co'
-    assert data_clubs[0]['points'] == "13"
-    assert data_clubs[1]['name'] == "Iron Temple"
-    assert data_clubs[1]['email'] == 'admin@irontemple.com'
-    assert data_clubs[1]['points'] == "4"
-    assert data_clubs[2]['name'] == "She Lifts"
-    assert data_clubs[2]['email'] == 'kate@shelifts.co.uk'
-    assert data_clubs[2]['points'] == "12"
-
-
-def test_load_competitions_file():
-    # Test load function return true values 
-    data_competitions = loadCompetitions()
-    assert data_competitions[0]['name'] == "Winter Meeting"
-    assert data_competitions[0]['date'] == "2020-04-20 15:00:00"
-    assert data_competitions[0]['numberOfPlaces'] == "28"
-    assert data_competitions[1]['name'] == "HeroesLifter"
-    assert data_competitions[1]['date'] == "2022-11-20 18:30:00"
-    assert data_competitions[1]['numberOfPlaces'] == "18"
-
-
-# ====Test local Fonction load_clubs() & load_competitions() ====
-
-def test_local_loadClubs():
-    pass
-    
-def test_local_loadCompetitions():
-    pass
-"""
 # ================== Test load fct =====================
-"""
-json_file = opent('/users.json')
-json_payload = json.load(json_file)
-response = requests.post(url=base_url, headers=headers_test, json=json_payload)
-print(response.text)
-"""
 
 class TestLoadClubs(unittest.TestCase):
 
@@ -98,11 +57,12 @@ class TestLoadCompetitions(unittest.TestCase):
         self.assertEqual(loadCompetitions(), competitions)
 
 
-# =============== Test booking page ==================
+# =============== Test access pages ==================
+def test_book_page_access_function(client, mocker):
 
-def test_book_page_access_function(client):
+    mocker.patch.object(server, "clubs", mock_clubs)
 
-    email = server.loadClubs()[0]['email']
+    email = "heracles@heavylift.gr"
     response = client.post("/showSummary", data={"email": email})
 
     data = response.data.decode()
@@ -112,10 +72,12 @@ def test_book_page_access_function(client):
     assert soup.h2.get_text() == 'Welcome,' + ' ' + email + ' '
 
 
-def test_purchase_page_access_function(client):
-    club_name = server.loadClubs()[1]['name']
-    comp_name = server.loadCompetitions()[1]['name']
+def test_purchase_page_access_function(client, mocker):
+    mocker.patch.object(server, "clubs", mock_clubs)
+    mocker.patch.object(server, "competitions", mock_comps)
 
+    club_name = mock_clubs[0]['name']
+    comp_name = mock_comps[0]['name']
     response = client.get('/book/' + comp_name + '/' + club_name)
 
     assert response.status_code == 200
@@ -124,20 +86,117 @@ def test_purchase_page_access_function(client):
     soup = BeautifulSoup(data, "html.parser")
     assert soup.h2.get_text() == comp_name
 
- # ==== test booking messages of usage of points and places ====
-"""
-def test_allocation_points(client):
 
+# ==== test booking messages of usage of points and places ====
+def test_over_booked_places(client, mocker):
+    mocker.patch.object(server, "clubs", mock_clubs)
+    mocker.patch.object(server, "competitions", mock_comps)
 
-    c = server.loadClubs()[1]('name')
-    comp = server.loadCompetitions()[1]['name']
-    response = client.get('/purchasePlaces', data={'points': points})
+    club_name = mock_clubs[0]['name']
+    comp_name = mock_comps[2]['name']
+    mock_places = int(mock_comps[2]['numberOfPlaces'])
+
+    placesRequired = 12
+
+    response = client.post("/purchasePlaces", data={
+        "club": club_name,
+        "competition": comp_name,
+        "places": placesRequired})
     assert response.status_code == 200
-    print('response:', response)
-    
-    if int(club['points']) > 12:
-        assert "You can't book more than 12 places! Retry." in data.response.decode()
-"""
+
+    data = response.data.decode()
+    print('Places disponibles:', mock_places)
+    print('booked:', placesRequired)
+
+    if int(placesRequired) > mock_places:
+        print('Not enough places ! Retry.')
+        assert "Not enough places ! Retry." in data
+    else:
+        print('Great-booking complete!')
+        assert("Great-booking complete!") in data
+
+
+def test_over_loaded_points(client, mocker):
+    mocker.patch.object(server, "clubs", mock_clubs)
+    mocker.patch.object(server, "competitions", mock_comps)
+
+    club_name = mock_clubs[2]['name']
+    comp_name = mock_comps[0]['name']
+    mock_points = int(mock_clubs[2]['points'])
+
+    placesRequired = 15
+
+    response = client.post("/purchasePlaces", data={
+        "club": club_name,
+        "competition": comp_name,
+        "places": placesRequired})
+    assert response.status_code == 200
+
+    data = response.data.decode()
+    print('Points disponibles:', mock_points)
+    print('booked:', placesRequired)
+
+    if int(placesRequired) > mock_points:
+        print("Not enough points ! Retry.")
+        # soup = BeautifulSoup(data, "html.parser")
+        assert "Not enough points ! Retry." in data
+
+def test_retry_points(client, mocker):
+
+    mocker.patch.object(server, "clubs", mock_clubs)
+    mocker.patch.object(server, "competitions", mock_comps)
+
+    club_name = mock_clubs[0]['name']
+    comp_name = mock_comps[0]['name']
+    mock_points = mock_clubs[0]['points']
+    print('mock_points:', mock_points)
+    max_booking_places = 12
+    placesRequired = 12
+
+    response = client.post("/purchasePlaces", data={
+        "club": club_name,
+        "competition": comp_name,
+        "places": placesRequired})
+    assert response.status_code == 200
+
+    data = response.data.decode()
+    print('Points disponibles:', mock_points)
+    print('booked:', placesRequired)
+
+    if int(placesRequired) <= max_booking_places:
+        assert "<li>Great-booking complete!</li>" in data
+        assert "Points available: 0" in data
+
+
+def test_over_limit_twelve_points(client, mocker):
+
+    mocker.patch.object(server, "clubs", mock_clubs)
+    mocker.patch.object(server, "competitions", mock_comps)
+
+    club_name = mock_clubs[2]['name']
+    comp_name = mock_comps[0]['name']
+    mock_points = mock_clubs[2]['points']
+    print('mock_points:', mock_points)
+    max_booking_places = 12
+    placesRequired = 13
+
+    response = client.post("/purchasePlaces", data={
+        "club": club_name,
+        "competition": comp_name,
+        "places": placesRequired})
+    assert response.status_code == 200
+
+    data = response.data.decode()
+    print('Points disponibles:', mock_points)
+    print('booked:', placesRequired)
+
+    if int(placesRequired) >= max_booking_places:
+        print('You can\'t book more than 12 places ! Retry.')
+        soup = BeautifulSoup(data, "html.parser")
+        assert soup.li.get_text() == "You can't book more than 12 places ! Retry."
+
+
+
 
 
 
